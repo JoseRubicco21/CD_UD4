09c4e582cf6c4bd6effd351bd0aad0a9860e85c7
JoseRubicco21 <a23joseRF@iessanclemente.net>
2024-04-12 11:55:09 +0200

    Added and fixed Anagrama Class

---
+++ app/src/main/java/ud4/e2/Anagrama.java
@@ -0,0 +1,67 @@
+package ud4.e2;
+
+import java.util.Arrays;  
+import java.util.Scanner;  
+      
+    class Anagrama {  
+    private String str1; 
+    private String str2;
+    private Scanner sc;
+    
+    public void Anagrama() {
+    	
+    }
+    
+    public void Anagrama(String str1, String str2) {
+    	this.str1=str1;
+    	this.str2=str2;
+    	this.sc = new Scanner(System.in);
+    }
+    
+    
+    public String getStr1() {
+		return str1;
+	}
+
+	public void setStr1(String str1) {
+		this.str1 = str1;
+	}
+
+	public String getStr2() {
+		return str2;
+	}
+
+	public void setStr2(String str2) {
+		this.str2 = str2;
+	}
+	
+	public void getInput() {
+		System.out.println("Introduce el primer dato a comprobar");
+		this.str1=sc.nextLine();
+		System.out.println("Introduce el segundo dato a comprobar");
+		this.str2=sc.nextLine();
+	};
+
+	public boolean checkAnagram(String str1, String str2) {
+ 
+    	if(str1.length() != str2.length()) return false;
+    	
+    	char[] charArray1 = str1.toCharArray();  
+        char[] charArray2 = str2.toCharArray();  
+        
+        Arrays.sort(charArray1);  
+        Arrays.sort(charArray2);  
+    	
+    	return Arrays.equals(charArray1, charArray2);
+    }
+    
+    public void printResult(boolean checkAnagramResult) {
+    	if(checkAnagramResult) {
+    		 System.out.println(this.str1 + " and " + this.str2 + " are anagram.");  
+     	} else {
+     		System.out.println(this.str1 + " and " + this.str2 + " are not anagram.");
+     	}
+    }
+    	
+  
+    }  
